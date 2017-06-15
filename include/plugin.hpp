00f0d3d4e8800ee2d569f1f7f948ae4ba7c335dd
¨Avra <edu.avraneel@gmail.com>
2017-06-15 16:11:39 +0530

    Added a plugin and plugin interface class.

---
+++ include/plugin.hpp
@@ -0,0 +1,97 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Tryx Plugin System made by Avra Neel Chakraborty                              //
+//                                                                               //
+// Copyright (c) 2017 Avra Neel Chakraborty                                      // 
+//                                                                               //
+// Permission is hereby granted, free of charge, to any person obtaining a copy  //
+// of this software and associated documentation files (the "Software"), to deal //
+// in the Software without restriction, including without limitation the rights  //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     //
+// copies of the Software, and to permit persons to whom the Software is         //
+// furnished to do so, subject to the following conditions:                      //
+//                                                                               //
+// The above copyright notice and this permission notice shall be included in    // 
+// all copies or substantial portions of the Software.                           // 
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PLUGIN_HPP
+#define PLUGIN_HPP
+
+#include "config.hpp"
+#include "sharedlib.hpp"
+
+#include <string>
+
+namespace Tryx {
+
+  // Representation of a plugin
+  class Plugin {
+    private:
+       //Signature for the plugin's registration function
+       typedef PluginInterface* *(*PluginFactoryFunc)();
+       //Signature to query for plugin texts
+       typedef char * (*Plugin_TextFunc)();
+       
+       //------------------------------------------------------------------- //
+       
+       //Handle of the loaded shared library
+       SharedLib::Handle sharedLibraryHandle;
+       //Number of references that exist to the shared library
+       size_t *referenceCount;
+       
+       char* filename;
+       char* pluginType;
+       char* pluginName;
+       char* pluginVersion;
+       PluginFactoryFunc funcHandle;
+       Plugin_TextFunc textHandle;
+       
+       void clearMembers();
+          
+       
+    public: 
+       //Initializes and loads a plugin
+       //Parameter is filename of the plugin to load
+       TRYX_API_EXP Plugin(const std::string &filename);
+       //Copies an existing plugin instance
+       TRYX_API_EXP Plugin(const Plugin &other);
+       //Unloads the plugin
+       TRYX_API_EXP ~Plugin();
+
+       //Queries the plugin for its expected engine version
+       TRYX_API_EXP int getEngineVersion() const {
+         return this->getEngineVersionAddress();
+       }
+       
+       TRYX_API_EXP Plugin_TextFunc* getTextData(const char* funcname,
+                                                     const std::string& filename);
+       TRYX_API_EXP PluginInterface* getNewPlugin(const char* funcname,
+                                                     const std::string& filename);
+       
+       TRYX_API_EXP void setFileName(char* name);
+       TRYX_API_EXP char* getName() { return pluginName; }
+       TRYX_API_EXP char* getType() { return pluginType; }
+       TRYX_API_EXP char* getVers() { return pluginVersion; }
+       TRYX_API_EXP void setName(char* name);
+       TRYX_API_EXP void setType(char* type);
+       TRYX_API_EXP void setVers(char* vers);
+         
+       //Creates a copy of the plugin instance
+       Plugin &operator =(const Plugin &other);
+    
+  };
+
+  // ----------------------------------------------------------------------- //
+
+} // namespace MyEngine
+
+#endif // MYENGINE_PLUGIN_H
+
+
+
+
+
+
+
+
+
---
+++ include/plugininterface.hpp
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Tryx Plugin System made by Avra Neel Chakraborty                              //
+//                                                                               //
+// Copyright (c) 2017 Avra Neel Chakraborty                                      // 
+//                                                                               //
+// Permission is hereby granted, free of charge, to any person obtaining a copy  //
+// of this software and associated documentation files (the "Software"), to deal //
+// in the Software without restriction, including without limitation the rights  //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     //
+// copies of the Software, and to permit persons to whom the Software is         //
+// furnished to do so, subject to the following conditions:                      //
+//                                                                               //
+// The above copyright notice and this permission notice shall be included in    // 
+// all copies or substantial portions of the Software.                           // 
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PLUGININTERFACE_HPP
+#define PLUGININTERFACE_HPP
+
+#include "sharedlib.hpp"
+#include "config.hpp"
+
+namespace Tryx{
+   class PluginInterface 
+   {
+      public:
+         PluginInterface(){}
+         virtual ~PluginInterface(){}
+
+         virtual char* onCommand(const char* data)=default;
+   };
+} //namespace Tryx
+
+#endif //define PLUGININTERFACE_HPP
+   
