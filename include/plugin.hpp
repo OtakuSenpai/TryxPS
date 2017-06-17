83edde0cdf2262fbe3eb452fdcd0acc75b3b9fd3
¨Avra <edu.avraneel@gmail.com>
2017-06-17 11:48:25 +0530

    Added documentation for the plugin class.Rewrote some functions too.

--- include/plugin.hpp
+++ include/plugin.hpp
@@ -14,6 +14,7 @@
 // all copies or substantial portions of the Software.                           // 
 ///////////////////////////////////////////////////////////////////////////////////
 
+
 #ifndef PLUGIN_HPP
 #define PLUGIN_HPP
 
@@ -28,49 +29,65 @@ namespace Tryx {
   class Plugin {
     private:
        //Signature for the plugin's registration function
-       typedef PluginInterface* (*PluginFactoryFunc)();
+       typedef PluginInterface* *(*PluginFactoryFunc)();
        //Signature to query for plugin texts
        typedef char * (*Plugin_TextFunc)();
        
-       char* filename;
-       char* pluginType;
        char* pluginName;
+       char* pluginType;
        char* pluginVersion;
-       PluginFactoryFunc pluginHandle;
+       char* filename;
+       PluginFactoryFunc funcHandle;
        
-       TRYX_API_EXP void setName(char* name);
-       TRYX_API_EXP void setType(char* type);
-       TRYX_API_EXP void setVers(char* vers);
-       TRYX_API_EXP void setFileName(char* name);
-       
-       TRYX_API_EXP Plugin_TextFunc* getTextData(SharedLib::Handle handle,
-                                                 const char* funcname,
-                                                 const std::string& filename);
-       TRYX_API_EXP PluginInterface* getNewPlugin(SharedLib::Handle handle,
-                                                  const char* funcname,
-                                                  const std::string& filename);
+       // Handy function to clear the data members of a plugin object.
+       // Used in the dtor only.
        void clearMembers();
        
-    public: 
-       //Initializes and loads a plugin
-       //Parameter is filename of the plugin to load
-       TRYX_API_EXP Plugin(SharedLib::Handle handle,const std::string &filename);
-       //Copies an existing plugin instance
-       TRYX_API_EXP Plugin(const Plugin &other);
-       //Unloads the plugin
-       TRYX_API_EXP ~Plugin();
+       TRYX_API_EXP void setName(char* name); // Set name.
+       TRYX_API_EXP void setType(char* type); // Set type.
+       TRYX_API_EXP void setVers(char* vers); // Set version 
+       TRYX_API_EXP void setFileName(char* name); // Set filename  
        
-       TRYX_API_EXP char* getName() { return pluginName; }
-       TRYX_API_EXP char* getType() { return pluginType; }
-       TRYX_API_EXP char* getVers() { return pluginVersion; }
+    public: 
+       // Parameterized ctor to load a plugin dll and initiate it inside
+       // the class object.Used as the primary way of loading plugins
+       // Parameters are:-
+       // SharedLib::Handle handle - A handle which is passed from the 
+       // Kernal class.It helps in loading the function pointers.
+       // std::string& filename - The filename to the dynamic library.
+       TRYX_API_EXP Plugin(SharedLib::Handle& handle,std::string &filename);
+       
+       // Copy ctor for constructing a plugin from one that has already 
+       // been loaded.Required to provide correct semantics for storing 
+       // plugins in an STL map container.
+       TRYX_API_EXP Plugin(const Plugin &other);
+       
+       // Unloads the plugin, unloading its library when no more 
+       // references to it exist.
+       TRYX_API_EXP ~Plugin();
+
+       // Gets a text function handle from the dynamic library and 
+       // returns it. If found then returns it,else returns nullptr.
+       TRYX_API_EXP Plugin_TextFunc* getTextData(SharedLib::Handle handle,
+                                         const char* funcname,
+                                         const std::string& filename);
+       
+       // Gets a PluginInterface object from the dynamic library.
+       // If found,then returns it,else returns nullptr.
+       TRYX_API_EXP PluginInterface* getNewPlugin(SharedLib::Handle handle,
+                                          const char* funcname,
+                                          const std::string& filename);
+                                          
+       TRYX_API_EXP char* getName() { return pluginName; } // Get name.
+       TRYX_API_EXP char* getType() { return pluginType; } // Get type.
+       TRYX_API_EXP char* getVers() { return pluginVersion; } // Get version.
+       TRYX_API_EXP char* getFilename() { return filename; } // Get filename. 
          
-       //Creates a copy of the plugin instance
-       Plugin &operator =(const Plugin &other);
-    
+       //Creates a copy of the plugin instance.
+       TRYX_API_EXP Plugin &operator =(const Plugin &other);
+     
   };
-
-  // ----------------------------------------------------------------------- //
-
+ 
 } // namespace MyEngine
 
 #endif // MYENGINE_PLUGIN_H
